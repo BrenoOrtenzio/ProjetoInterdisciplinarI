6e8b297efc4488a2fdf5fb11573bcea85b0cd6b8
Breno Ortenzio <brenoortenzio@gmail.com>
2023-04-28 13:44:42 -0300

    first commit

---
+++ AgendaUsuario/index.js
@@ -0,0 +1,75 @@
+
+const calendario = document.querySelector('.calendario');
+const tituloMes = document.querySelector('#titulo-mes');
+const mesAnterior = document.querySelector('#mes-anterior');
+const mesProximo = document.querySelector('#mes-proximo');
+const tabelaCalendario = document.querySelector('#tabela-calendario');
+const tbodyCalendario = document.querySelector('#tabela-calendario tbody');
+
+let data = new Date();
+
+mesAnterior.addEventListener('click', () => {
+    data.setMonth(data.getMonth() - 1);
+    renderizarCalendario();
+});
+
+mesProximo.addEventListener('click', () => {
+    data.setMonth(data.getMonth() + 1);
+    renderizarCalendario();
+});
+
+function renderizarCalendario() {
+    const mesAtual = data.getMonth();
+    const anoAtual = data.getFullYear();
+    const diasDoMes = new Date(anoAtual, mesAtual + 1, 0).getDate();
+    const primeiroDiaSemana = new Date(anoAtual, mesAtual, 1).getDay();
+
+    tituloMes.innerHTML = `${meses[mesAtual]} ${anoAtual}`;
+
+    tbodyCalendario.innerHTML = '';
+
+    let diaAtual = 1;
+
+    for (let i = 0; i < 6; i++) {
+        const linha = document.createElement('tr');
+
+        for (let j = 0; j < 7; j++) {
+            const celula = document.createElement('td');
+
+            if (i === 0 && j < primeiroDiaSemana) {
+                const textoCelula = document.createTextNode('');
+                celula.appendChild(textoCelula);
+            } else if (diaAtual > diasDoMes) {
+                break;
+            } else {
+                const textoCelula = document.createTextNode(diaAtual);
+                celula.appendChild(textoCelula);
+                diaAtual++;
+            }
+
+            linha.appendChild(celula);
+        }
+
+        tbodyCalendario.appendChild(linha);
+    }
+}
+
+const meses = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+];
+
+
+renderizarCalendario();
+
+
---
+++ Login/index.js
@@ -0,0 +1,78 @@
+function onChangeEmail() {
+  toggleButtonsDisable();
+  toggleEmailErrors();
+}
+
+function onChangePassword() {
+  toggleButtonsDisable();
+  togglePasswordErrors();
+}
+
+function login() {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(form.email().value, form.password().value)
+    .then((response) => {
+      window.location.href = "/HomePage/index.html";
+    })
+    .catch((error) => {
+      alert(getErrorMessage(error));
+    });
+}
+
+function getErrorMessage(error) {
+  if (error.code == "auth/user-not-found") {
+    return "Usuário nao encontrado";
+  }
+  return error.message;
+}
+
+function register() {
+  window.location.href = "pages/register/register.html";
+}
+
+function toggleEmailErrors() {
+  const email = form.email().value;
+  form.emailRequiredError().style.display = email ? "none" : "block";
+
+  form.emailInvalidError().style.display = validateEmail(email)
+    ? "none"
+    : "block";
+}
+
+function togglePasswordErrors() {
+  const password = form.password().value;
+  form.passwordRequiredError().style.display = password ? "none" : "block";
+}
+
+function toggleButtonsDisable() {
+  const emailValid = isEmailValid();
+  form.recoverPasswordButton().disabled = !emailValid;
+
+  const passwordValid = isPasswordValid();
+  form.loginButton().disabled = !emailValid || !passwordValid;
+}
+
+function isEmailValid() {
+  const email = form.email().value;
+  if (!email) {
+    return false;
+  }
+  return validateEmail(email);
+}
+
+function isPasswordValid() {
+  return form.password().value ? true : false;
+}
+
+const form = {
+  email: () => document.getElementById("email"),
+  emailInvalidError: () => document.getElementById("email-invalid-error"),
+  emailRequiredError: () => document.getElementById("email-required-error"),
+  loginButton: () => document.getElementById("login-button"),
+  password: () => document.getElementById("password"),
+  passwordRequiredError: () =>
+    document.getElementById("password-required-error"),
+  recoverPasswordButton: () =>
+    document.getElementById("recover-password-button"),
+};
